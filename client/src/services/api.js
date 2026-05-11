96f08f3d4e33642b17e2fd195374e7218dbbda52
Tanvi Tyagi <tanvityagi694@gmail.com>
2026-05-11 16:26:43 +0530

    add vercel config

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://team-task-manager-production-b85f.up.railway.app/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default api;
