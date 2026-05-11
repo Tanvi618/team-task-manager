b764d625c486886d815b45609917cff0c5f6de9a
Tanvi Tyagi <tanvityagi694@gmail.com>
2026-05-11 12:22:51 +0530

    fix task model missing issue

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://team-task-manager-production-b85f.up.railway.app/api",
 });
 
 export default api;
